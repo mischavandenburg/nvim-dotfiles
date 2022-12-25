4a3970aac41e2f2476e51eda2e18eee9ee50f43f
Mischa <mischavandenburg@gmail.com>
2022-12-25 09:38:24 +0100

    added some keybinds and settings

--- after/plugin/settings.lua
+++ after/plugin/settings.lua
@@ -10,3 +10,14 @@ g.gruvbox_material_enable_italic_comment = 1
 
 -- undo
 vim.opt.undodir = vim.fn.expand('~/.nvim/undodir')
+
+vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
+
+-- these keep the cursor in the middle when scrolling with ctrl d and u
+-- from https://github.com/ThePrimeagen/init.lua
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- and these are for searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
