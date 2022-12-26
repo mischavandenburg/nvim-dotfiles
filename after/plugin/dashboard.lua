625e906ab1b24df917f1a8c2d01a287f9a7e8c13
Mischa <mischavandenburg@gmail.com>
2022-12-26 20:36:17 +0100

    pimped dashboard

--- after/plugin/dashboard.lua
+++ after/plugin/dashboard.lua
@@ -1,47 +1,37 @@
-  local home = os.getenv('HOME')
-  local db = require('dashboard')
-  -- macos
-  -- db.preview_command = 'cat | lolcat -F 0.3'
-  -- linux
-  --- db.preview_command = 'ueberzug'
-  --
-  -- db.preview_file_path = home .. '/.config/nvim/static/neovim.cat'
-  --
-  db.default_banner = {
-      "",
-      "",
-      " ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
-      " ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
-      " ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
-      " ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
-      " ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
-      " ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
-      "",
-      " [ TIP: To exit Neovim, just power off your computer. ] ",
-      "",
-  }
+-- local home = os.getenv('HOME')
+local db = require('dashboard')
+-- macos
+-- db.preview_command = 'cat | lolcat -F 0.3'
+-- linux
+--- db.preview_command = 'ueberzug'
+--
+-- db.preview_file_path = home .. '/.config/nvim/static/neovim.cat'
+--
+db.default_banner = {
+    "",
+    "",
+    " ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
+    " ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
+    " ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
+    " ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
+    " ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
+    " ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
+    "",
+}
 
-  db.preview_file_height = 11
-  db.preview_file_width = 70
-  db.custom_center = {
-      {icon = '  ',
-      desc = 'Recently latest session                  ',
-      shortcut = 'SPC s l',
-      action ='SessionLoad'},
-      {icon = '  ',
-      desc = 'Recently opened files                   ',
-      action =  'Telescope oldfiles',
-      shortcut = 'SPC f h'},
-      {icon = '  ',
-      desc = 'Find  File                              ',
-      action = 'Telescope find_files find_command=rg,--hidden,--files',
-      shortcut = 'SPC f f'},
-      {icon = '  ',
-      desc ='File Browser                            ',
-      action =  'Telescope file_browser',
-      shortcut = 'SPC f b'},
-      {icon = '  ',
-      desc = 'Open Personal dotfiles                  ',
-      action = 'file ~/.config/nvim/init.lua',
-      shortcut = 'SPC f d'},
-    }
+-- db.preview_file_height = 11
+-- db.preview_file_width = 70
+db.custom_center = {
+    {icon = '  ',
+        desc = 'Recently opened files                   ',
+        action =  'Telescope oldfiles',
+        shortcut = '␣ s r'},
+    {icon = '  ',
+        desc = 'Find  File                              ',
+        action = 'Telescope find_files find_command=rg,--hidden,--files',
+        shortcut = '␣ s f'},
+    {icon = '  ',
+        desc ='File Browser toggle                     ',
+        action =  'NvimTreeToggle',
+        shortcut = '␣ f t'},
+}
