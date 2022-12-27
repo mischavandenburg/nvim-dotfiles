d50aa0a598d0906088c9cb55f7aafa08fe3874bb
Mischa <mischavandenburg@gmail.com>
2022-12-27 12:31:57 +0100

    adjusted run python keybind and finished dashboard

--- after/plugin/dashboard.lua
+++ after/plugin/dashboard.lua
@@ -1,6 +1,16 @@
 local db = require('dashboard')
 local quote = "If you do only easy things, your life will be hard. If you do hard things, your life will become easy. - Les Brown"
 
+local second_brain = function ()
+    vim.cmd('cd ~/obsidian/second-brain/')
+    vim.cmd('Telescope find_files find_command=rg,--hidden,--files')
+    end
+
+local edit_vim_dotfiles = function ()
+    vim.cmd('cd ~/.config/nvim/')
+    vim.cmd('Telescope find_files find_command=rg,--hidden,--files')
+    end
+
 db.default_banner = {
     "",
     "",
@@ -18,15 +28,23 @@ db.default_banner = {
 }
 
 db.custom_center = {
-    {icon = '  ',
+    {icon = '📁  ',
         desc = 'Recently opened files                   ',
         action =  'Telescope oldfiles',
         shortcut = '␣ s r'},
-    {icon = '  ',
+    {icon = '🧠  ',
+        desc = 'Open Second Brain                       ',
+        action = second_brain,
+        shortcut = '␣ f b'},
+    {icon = '🗒️  ',
+        desc = 'Edit vim dotfiles                       ',
+        action = edit_vim_dotfiles,
+        shortcut = '␣ f v'},
+    {icon = '🗄️  ',
         desc = 'Find  File                              ',
         action = 'Telescope find_files find_command=rg,--hidden,--files',
         shortcut = '␣ s f'},
-    {icon = '  ',
+    {icon = '🌳  ',
         desc ='File Browser toggle                     ',
         action =  'NvimTreeToggle',
         shortcut = '␣ f t'},
--- after/plugin/keybinds.lua
+++ after/plugin/keybinds.lua
@@ -12,7 +12,8 @@ map('n', 'Y', '"+y')
 map('v', 'Y', '"+y')
 
 -- Python: run current buffer 
-map('n', '<leader>rr', '<cmd>w<cr><cmd>split term://python3 %<cr>')
+-- map('n', '<leader>rr', '<cmd>w<cr><cmd>term://python3 %<cr>')
+map('n', '<leader>rr', '<cmd>w<cr><cmd>:term python3 %<cr>')
 
 -- whitespaces
 -- remove whitespace from lines
@@ -27,12 +28,15 @@ vim.keymap.set('n', '<leader>u', '<cmd>UndotreeToggle<cr>', { desc = '[U]ndotree
 
 -- nvim-tree file explorer
 vim.keymap.set('n', '<leader>ft', '<cmd>NvimTreeToggle<cr>', { desc = '[F]ile tree [T]oggle' })
+vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<cr>', { desc = '[E]xplore files (toggle)' })
 vim.keymap.set('n', '<leader>ff', '<cmd>NvimTreeFocus<cr>', { desc = '[F]ile tree [F]ocus' })
 vim.keymap.set('n', '<leader>fc', '<cmd>NvimTreeFindFile<cr>', { desc = '[F]ile tree [C]urrent file focus' })
 
 -- file tree: open file tree in openstack repo and change directory to it
 vim.keymap.set('n', '<leader>fa', '<cmd>NvimTreeOpen ~/git/Openstack/<cr><cmd>cd ~/git/Openstack/<cr>', { desc = '[F]ile tree [A]msterdam' })
 vim.keymap.set('n', '<leader>fv', '<cmd>NvimTreeOpen ~/.config/nvim/<cr><cmd>cd ~/.config/nvim/<cr>', { desc = '[F]ile tree [V]im' })
+vim.keymap.set('n', '<leader>fb', '<cmd>NvimTreeOpen ~/obsidian/second-brain/<cr><cmd>cd ~/obsidian/second-brain/<cr>', { desc = '[F]ile tree [V]im' })
+
 
 -- markdown preview
 vim.keymap.set('n', '<leader>mp', '<cmd>MarkdownPreview<cr>', { desc = '[M]arkdown [P]review' })
