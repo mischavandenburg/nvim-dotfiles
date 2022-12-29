dbb6f596ae22e733df501035e47794a273451c45
Mischa <mischavandenburg@gmail.com>
2022-12-29 19:31:27 +0100

    custom indentation for yaml and lua

---
+++ after/ftplugin/lua.lua
@@ -0,0 +1,4 @@
+vim.o.expandtab = true              -- convert tabs to spaces
+vim.o.tabstop = 2                   -- insert 2 spaces for a tab
+vim.o.shiftwidth = 2                -- the number of spaces inserted for each indentation
+vim.o.smartindent = true
