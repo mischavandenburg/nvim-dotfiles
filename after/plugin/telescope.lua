f09cfeaf57a9958d8fa634f06480efda3058a57d
Mischa <mischavandenburg@gmail.com>
2023-01-06 06:53:54 +0100

    Added -L to follow sym links in Telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -38,3 +38,33 @@ require('telescope').setup{
 
 require('telescope').load_extension('fzf')
 -- require("telescope").load_extension("emoji")
+
+-- Custom ripgrep configuration:
+
+local telescope = require("telescope")
+local telescopeConfig = require("telescope.config")
+
+-- Clone the default Telescope configuration
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+-- I want to search in hidden/dot files.
+table.insert(vimgrep_arguments, "--hidden")
+-- I don't want to search in the `.git` directory.
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
+-- I want to follow symbolic links
+table.insert(vimgrep_arguments, "-L")
+
+telescope.setup({
+	defaults = {
+		-- `hidden = true` is not supported in text grep commands.
+		vimgrep_arguments = vimgrep_arguments,
+	},
+	pickers = {
+		find_files = {
+			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*", "-L" },
+		},
+	},
+})
