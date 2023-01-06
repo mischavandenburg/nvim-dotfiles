627c8321c4c881b9c837ba0834a8f54053408839
Mischa <mischavandenburg@gmail.com>
2023-01-06 07:40:01 +0100

    added vim & terraform to treesitter

--- init.lua
+++ init.lua
@@ -222,7 +222,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'yaml', 'markdown' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'yaml', 'markdown', 'vim', 'terraform' },
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python', 'yaml' } },
